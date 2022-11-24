e2cf8c1e10f04e30bf9661151c2f061c0184c495
Juan David <juandavid918@gmail.com>
2022-11-23 19:35:54 -0500

    Crear y listar tareas en proceso app tareas por hacer

--- 04. Tareas hacer/app.js
+++ 04. Tareas hacer/app.js
@@ -1,11 +1,36 @@
 require("colors");
 
-const { mostrarMenu } = require("./helpers/mensajes");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
+
+//const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
 console.clear();
 
 const main = async() => {
     console.log("Hola mundo");
+
+    let opt = "";
+    const tareas = new Tareas();
+
+    do {
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case "1":
+                const desc = await leerInput("Descripción:");
+
+                tareas.crearTarea(desc);
+
+                break;
+
+            case "2":
+                console.log(tareas._listado);
+                break;
+        }
+
+        await pausa();
+    } while (opt !== "0");
 
     mostrarMenu();
 };
