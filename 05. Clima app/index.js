07091d29090068a61b84900ab5d22732658e23b6
Juan David <juandavid918@gmail.com>
2022-12-09 12:12:31 -0500

    Clima app finalizada, introduccion a Express en proceso

--- 05. Clima app/index.js
+++ 05. Clima app/index.js
@@ -11,7 +11,6 @@ const main = async() => {
         console.clear();
 
         opt = await inquirerMenu();
-        console.log(opt);
 
         switch (opt) {
             case 1:
@@ -23,18 +22,36 @@ const main = async() => {
 
                 // Seleccionar el lugar
                 const id = await listarLugares(lugares);
+
+                if (id === "0") {
+                    continue;
+                }
+
                 const lugarSel = lugares.find(l => l.id === id);
 
+                // Guardar en DB
+                busquedas.agregarHistorial(lugarSel.nombre);
+
                 // Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
 
                 // Mostrar resultados
+                console.clear();
                 console.log("\nInformación de la ciudad".green);
-                console.log("Ciudad:", lugarSel.nombre);
+                console.log("Ciudad:", lugarSel.nombre.green);
                 console.log("Lat:", lugarSel.lat);
                 console.log("Lng:", lugarSel.lng);
-                console.log("Temperatura:", );
-                console.log("Mínima:", );
-                console.log("Máxima:", );
+                console.log("Temperatura:", clima.temp);
+                console.log("Mínima:", clima.min);
+                console.log("Máxima:", clima.max);
+                console.log("Como está el clima:", clima.desc.green);
+                break;
+
+            case 2:
+                busquedas.historialCapitalizado.forEach((lugar, i) => {
+                    const idx = `${ i + 1 }.`.green;
+                    console.log(`${ idx } ${ lugar }`);
+                });
                 break;
         }
 
