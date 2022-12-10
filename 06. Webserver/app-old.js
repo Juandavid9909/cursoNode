4d78db65da0f9e33b48d4e4626c783263984a916
Juan David <juandavid918@gmail.com>
2022-12-10 12:55:26 -0500

    Webserver en proceso, configurando handlebars

---
+++ 06. Webserver/app-old.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+        // res.setHeader("Content-Disposition", "attachment; filename=lista.csv");
+        // res.writeHead(200, { "Content-Type": "application/csv" });
+
+        // res.write("id, nombre\n");
+        // res.write("1, Fernando\n");
+        // res.write("2, Maria\n");
+        // res.write("3, Juan\n");
+        // res.write("4, Pedro\n");
+
+
+        // res.writeHead(200, { "Content-Type": "application/json" });
+
+        // const persona = {
+        //     id: 1,
+        //     nombre: "Juan"
+        // }
+
+        // res.write(JSON.stringify(persona));
+
+        res.write("Hola Mundo")
+        res.end();
+    })
+    .listen(8080);
+
+console.log("Escuchando el puerto", 8080);
--- 06. Webserver/app.js
+++ 06. Webserver/app.js
@@ -1,9 +1,32 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-http.createServer((req, res) => {
-        res.write("Hola Mundo");
-        res.end();
-    })
-    .listen(8080);
+const port = 8080;
 
-console.log("Escuchando el puerto", 8080);
+app.set("view engine", "hbs");
+
+// Servir contenido estático
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home", {
+        nombre: "Juan David",
+        titulo: "Curso de Node"
+    });
+});
+
+app.get("/generic", (req, res) => {
+    res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.get("/elements", (req, res) => {
+    res.sendFile(__dirname + "/public/elements.html");
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/public/404.html");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${ port }`);
+});
