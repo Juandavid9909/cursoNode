e87c0ad5d6a95a7b17a16ce4d71e55aa6f84b75a
Juan David <juandavid918@gmail.com>
2023-01-07 12:09:16 -0500

    Creando conexion con mongoose en el Restserver

--- 07. Restserver/models/server.js
+++ 07. Restserver/models/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 
+const { dbConnection } = require("../database/config");
+
 class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = "/api/usuarios";
+
+        // Conectar a base de datos
+        this.conectarDB();
 
         // Middlewares
         this.middlewares();
@@ -15,9 +20,16 @@ class Server {
         this.routes();
     }
 
+    async conectarDB() {
+        await dbConnection();
+    }
+
     middlewares() {
         // CORS
         this.app.use(cors());
+
+        // Lectura y parseo del body
+        this.app.use(express.json());
 
         // Directorio público
         this.app.use(express.static("public"));
