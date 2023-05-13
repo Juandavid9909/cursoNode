a97d23af605919ded3a56da5aa9e80776328f41d
Juan David <juandavid918@gmail.com>
2023-05-13 09:37:53 -0500

    Aplicacion de colas finalizada

--- 09. Socket colas/sockets/controller.js
+++ 09. Socket colas/sockets/controller.js
@@ -1,9 +1,47 @@
-const socketController = (socket) => {
-    socket.on('enviar-mensaje', (payload, callback) => {
-        const id = 123456789;
-        callback(id);
+const TicketControl = require("../models/ticket-control");
 
-        socket.broadcast.emit('enviar-mensaje', payload);
+const ticketControl = new TicketControl();
+
+const socketController = (socket) => {
+    // Cuando un cliente se conecta
+    socket.emit("ultimo-ticket", ticketControl.ultimo);
+    socket.emit("estado-actual", ticketControl.ultimos4);
+    socket.emit("tickets-pendientes", ticketControl.tickets.length);
+
+    socket.on('siguiente-ticket', (payload, callback) => {
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+
+        // TODO: Notificar que hay un nuevo ticket pendiente de asignar
+        socket.broadcast.emit("tickets-pendientes", ticketControl.tickets.length);
+    });
+
+    socket.on("atender-ticket", ({ escritorio }, callback) => {
+        if (!escritorio) {
+            return callback({
+                ok: false,
+                msg: "El escritorio es obligatorio"
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket(escritorio);
+
+        // TODO: Notificar cambio en los últimos 4
+        socket.broadcast.emit("estado-actual", ticketControl.ultimos4);
+        socket.emit("tickets-pendientes", ticketControl.tickets.length);
+        socket.broadcast.emit("tickets-pendientes", ticketControl.tickets.length);
+
+        if (!ticket) {
+            callback({
+                ok: false,
+                msg: "Ya no hay tickets pendientes"
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket
+            });
+        }
     });
 }
 
